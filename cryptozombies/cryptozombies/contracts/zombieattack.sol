e1464435a85fe3aa8d033b76e45f86a33c35d87c
sujantkumarkv <kumarskv.201cv253@nitk.edu.in>
2021-11-12 03:29:18 +0530

    add cryptozombies directory to store the proper truffle file directory system

---
+++ cryptozombies/cryptozombies/contracts/zombieattack.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "./zombiehelper.sol";
+
+contract ZombieAttack is ZombieHelper {
+  uint randNonce = 0;
+  uint attackVictoryProbability = 70;
+
+  function randMod(uint _modulus) internal returns(uint) {
+    //we can use OpenZepplin's SafeMath library since it was imported in zombiefactory which is base contract 
+    // & other contracts inherit it so ya ...
+    randNonce = randNonce.add(1);
+    return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
+  }
+
+  function attack(uint _zombieId, uint _targetId) external onlyOwnerOf(_zombieId) {
+    Zombie storage myZombie = zombies[_zombieId];
+    Zombie storage enemyZombie = zombies[_targetId];
+    uint rand = randMod(100);
+    if (rand <= attackVictoryProbability) {
+      myZombie.winCount = myZombie.winCount.add(1);
+      myZombie.level = myZombie.level.add(1);
+      enemyZombie.lossCount = enemyZombie.lossCount.add(1);
+      feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
+    } else {
+      myZombie.lossCount = myZombie.lossCount.add(1);
+      enemyZombie.winCount = enemyZombie.winCount.add(1);
+      _triggerCooldown(myZombie);
+    }
+  }
+}
